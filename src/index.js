9cda13a3d9af95e4ae3cbd07da4589b80d40daa7
Web-proger <guser999@mail.ru>
2020-11-04 19:30:18 +0700

    feat(index) tmp - перенос между компами

--- src/index.js
+++ src/index.js
@@ -2,19 +2,33 @@ import 'bootstrap';
 import axios from 'axios';
 import yup from 'yup';
 import onChange from 'on-change';
-import corsProxy from 'cors-anywhere';
+
+const PROXY_URL = 'https://cors-anywhere.herokuapp.com';
 
 const form = document.querySelector('.rss-form');
 const button = form.querySelector('#submit-button');
 const inputField = form.querySelector('#rss-input');
 
+// http://feeds.feedburner.com/css-live   - ёщё RSS
+inputField.value = 'https://ru.hexlet.io/lessons.rss';  // Для тестирования
+
+const state = {
+  feeds: [],
+  posts: [],
+}
+
+
 const handleSubmit = (evt) => {
   evt.preventDefault();
+  const rssUrl = inputField.value;
+  const url = encodeURI(`${PROXY_URL}/${rssUrl}`);
+  console.log(url);
 
-  axios.get('https://ru.hexlet.io/lessons.rss')
-    .then((data) => {
+  axios.get(url)
+    .then((response) => {
       const parser = new DOMParser();
-      const doc = parser.parseFromString(data, 'application/xml');
+      const doc = parser.parseFromString(response.data, 'application/xml');
+      console.log(doc.querySelector('item'));
       console.log(doc);
     })
     .catch((err) => {
