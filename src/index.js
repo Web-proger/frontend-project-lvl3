903a8675ed31cded2b250145cdfbd5fe83d69c58
Ivan Tarasov <guser999@mail.ru>
2020-11-11 16:48:43 +0700

    refactor(index.js) Добавление одной ссылки

--- src/index.js
+++ src/index.js
@@ -35,6 +35,62 @@ const handleSubmit = (evt) => {
         link: el.querySelector('link').textContent,
       }));
 
+      const feeds = document.querySelector('.feeds');
+      const posts = document.querySelector('.posts');
+
+      // Заголовок Feeds
+      const feedsTitle = document.createElement('h2');
+      feedsTitle.textContent = 'Feeds';
+
+      // Список ul
+      const list = document.createElement('ul');
+      list.classList.add('list-group', 'mb-5');
+
+      // Элементы списка li
+      const listItem = document.createElement('li');
+      listItem.classList.add('list-group-item');
+      const listItemTitle = document.createElement('h3');
+      listItemTitle.textContent =data.feed;
+      const listItemDescription = document.createElement('p');
+      listItemDescription.textContent = data.description;
+
+      // Формирую список
+      listItem.appendChild(listItemTitle);
+      listItem.appendChild(listItemDescription);
+      list.appendChild(listItem);
+
+      // Формирую блок фидов
+      feeds.appendChild(feedsTitle);
+      feeds.appendChild(list);
+
+
+      // Заголовок Posts
+      const postsTitle = document.createElement('h2');
+      postsTitle.textContent = 'Posts';
+
+      // Список ul постов
+      const postList = document.createElement('ul');
+      postList.classList.add('list-group');
+
+      // Элементы списка постов li
+      data.posts.forEach(({link, title}) => {
+        const postsListItem = document.createElement('li');
+        postsListItem.classList.add('list-group-item');
+
+        const postsLink = document.createElement('a');
+        postsLink.textContent = title;
+        postsLink.href = link;
+
+        postsListItem.appendChild(postsLink);
+        postList.appendChild(postsListItem);
+
+      })
+
+      posts.appendChild(postsTitle);
+      posts.appendChild(postList);
+
+
+
       console.log(doc);
       console.log(data);
     })
