e5d99fdd725e057c8ed4165d5e2a91b542b7d6be
Ivan Tarasov <guser999@mail.ru>
2020-11-13 19:38:56 +0700

    refactor(en.js) тексты интерфейса на англ, для i18next

---
+++ src/en.js
@@ -0,0 +1,17 @@
+export default {
+  en: {
+    translation: {
+      title: 'RSS Reader',
+      description: 'Start reading RSS today! It is easy, it is nicely.',
+      example: 'Example: https://ru.hexlet.io/lessons.rss',
+      buttonText: 'Add',
+      footerText: 'created by',
+      footerLinkText: 'Hexlet',
+      message: {
+        successMessage: 'Rss has been loaded',
+        urlExists: 'Rss already exists',
+        noValidUrl: 'Must be valid url',
+      },
+    },
+  },
+};
