98f1066ee68fb373c4c25f95671cbcf8e4f96f53
Ivan Tarasov <guser999@mail.ru>
2020-12-05 17:09:28 +0700

    refactor(init.js) Обаботчики вынес в функции

--- src/init.js
+++ src/init.js
@@ -6,6 +6,28 @@ import addRss from './addRss';
 import updateRss from './updateRss';
 import config from './config';
 
+const updateData = (evt, state) => {
+  const watchedState = state;
+  const previewId = Number(evt.target.id);
+  const post = watchedState.posts.find((el) => el.postId === previewId);
+
+  if (post === 'undefined') return;
+
+  post.isViewed = true;
+
+  watchedState.modal = {
+    title: post.title,
+    description: post.description,
+    link: post.link,
+  };
+  watchedState.posts = [...watchedState.posts];
+};
+
+const changeLanguage = (evt, state) => {
+  const watchedState = state;
+  watchedState.language = evt.target.dataset.language;
+};
+
 export default () => {
   i18next.init({
     lng: config.defaultLanguage,
@@ -37,24 +59,9 @@ export default () => {
       // Отправка формы
       document.querySelector('.rss-form').addEventListener('submit', (evt) => addRss(evt, watchedState));
       // Открытие модального окна
-      document.querySelector('.posts').addEventListener('click', (evt) => {
-        const previewId = Number(evt.target.id);
-        const post = watchedState.posts.find((el) => el.postId === previewId);
-
-        if (post === 'undefined') return;
-
-        post.isViewed = true;
-        watchedState.modal = {
-          title: post.title,
-          description: post.description,
-          link: post.link,
-        };
-        watchedState.posts = [...watchedState.posts];
-      });
+      document.querySelector('.posts').addEventListener('click', (evt) => updateData(evt, watchedState));
       // Переключение языков
-      document.querySelector('#buttons').addEventListener('click', (evt) => {
-        watchedState.language = evt.target.dataset.language;
-      });
+      document.querySelector('#buttons').addEventListener('click', (evt) => changeLanguage(evt, watchedState));
 
       setTimeout(() => updateRss(watchedState), config.updateTime);
 
