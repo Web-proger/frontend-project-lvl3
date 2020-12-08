94cb911f990c4df2f098f5f21a3b36a892634e7e
Web-proger <guser999@mail.ru>
2020-12-08 22:43:35 +0700

    feat(init.js) перенёс модуль addRss.js в виде функции в init.js

--- src/init.js
+++ src/init.js
@@ -1,11 +1,49 @@
 import i18next from 'i18next';
 import onChange from 'on-change';
+import axios from 'axios';
 import { string } from 'yup';
+import _ from 'lodash';
 import watch from './watch';
 import resources from './locales';
-import addRss from './addRss';
 import updateRss from './updateRss';
+import parse from './parser';
 import config from './config';
+
+const addRss = (rssUrl, state) => {
+  const watchedState = state;
+
+  const url = `${config.proxy}/${rssUrl}`;
+  watchedState.form.status = 'sending';
+
+  // Запрос с указанным урлом
+  return axios.get(url)
+    // Формирование списка Постов и Фидов
+    .then((response) => {
+      const { posts, description, title } = parse(response.data);
+      const feedId = _.uniqueId();
+      const rssPosts = posts.map((item) => ({
+        ...item,
+        feedId,
+        isViewed: false,
+        postId: _.uniqueId(),
+      }));
+
+      watchedState.form.status = 'input';
+      watchedState.form.feedback = i18next.t('message.successMessage');
+
+      watchedState.feeds.unshift({
+        title,
+        description,
+        link: rssUrl,
+        id: feedId,
+      });
+      watchedState.posts.unshift(...rssPosts);
+    })
+    .catch((err) => {
+      watchedState.feedback = err.message;
+      watchedState.status = 'error';
+    });
+};
 
 const handleSubmit = (evt, state) => {
   evt.preventDefault();
