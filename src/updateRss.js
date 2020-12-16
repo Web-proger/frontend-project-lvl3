25eaa1df785129c020f5b4b82ddedc3f4fba276d
Ivan Tarasov <guser999@mail.ru>
2020-12-16 17:24:10 +0700

    refactor(updateRss.js) Рефактор

--- src/updateRss.js
+++ src/updateRss.js
@@ -8,25 +8,21 @@ const updateRss = (state) => {
   const watchedState = state;
 
   const promises = watchedState.feeds.map((feed) => axios.get(`${config.proxy}/${feed.link}`)
+    .then((response) => {
+      const { posts: newPosts } = parse(response.data);
+      const oldPosts = watchedState.posts.filter((post) => post.feedId === feed.id);
+
+      return _.differenceWith(newPosts, oldPosts, (a, b) => a.title === b.title)
+        .map((post) => ({ id: _.uniqueId(), feedId: feed.id, ...post }));
+    })
     .catch((err) => {
       console.log(err.message);
       return [];
     }));
 
   Promise.all(promises)
-    .then((responses) => {
-      const uniquePosts = responses.map((response, i) => {
-        const feed = watchedState.feeds[i];
-        const { posts: newPosts } = parse(response.data);
-        const oldPosts = watchedState.posts.filter((post) => post.feedId === feed.id);
-
-        return _.differenceWith(newPosts, oldPosts, (a, b) => a.title === b.title)
-          .map((post) => ({ id: _.uniqueId(), feedId: feed.id, ...post }));
-      });
-
-      if (_.isEmpty(uniquePosts)) return;
-
-      watchedState.posts.unshift(...uniquePosts.flat());
+    .then((posts) => {
+      watchedState.posts.unshift(...posts.flat());
     })
     .catch((err) => {
       throw new Error(err.message);
